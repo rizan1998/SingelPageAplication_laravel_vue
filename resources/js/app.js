8b7f90b6401499ab5376c4f311f4f6803559192b
rizan1998 <rizanalfalah@gmail.com>
2021-02-10 10:44:52 +0700

    rombak file app untuk tidak menggunakan loadash, menambah package vue router dan import vue router kedalam app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,33 +1,27 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
 
-require('./bootstrap');
+// jquery untuk navigation
+try {
+    window.Popper = require('popper.js').default;
+    window.$ = window.jQuery = require('jquery');
 
-window.Vue = require('vue').default;
+    require('bootstrap');
+} catch (e) { }
 
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
+// axios
+import axios from 'axios';
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+// require('./bootstrap');
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+// vue js
+window.Vue = require('vue').default; //vue app
+import VueRouter from 'vue-router'; //vue router
+
+Vue.use(VueRouter); //use vue router
+
+
 Vue.component('navigation', require('./components/Navigation.vue').default);
 
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
 
 const app = new Vue({
     el: '#parsinta',
