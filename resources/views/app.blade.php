83720cccc21c9078cf543df340f7cf7c865015fe
rizan1998 <38775582+rizan1998@users.noreply.github.com>
2021-02-19 13:19:12 +0700

    mengubah title application

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravell</title>
+        <title>Rest Api Laravel</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
